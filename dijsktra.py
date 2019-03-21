e22216572837ec695d3acd8a5c8879ef75d4ebd8
ganya7 <30626883+ganya7@users.noreply.github.com>
2019-03-21 23:18:26 +0530

    Add files via upload

--- dijsktra.py
+++ dijsktra.py
@@ -1,65 +1,177 @@
+import serial
+import time
+import cv2
+import threading
+import time
+
+
+import Inference as inf
+
 import heapq
 
-def dijkstra(g,d,source,end):
-	dist = [100]*len(g)
-	dist[source] = 0 #distance source vertex = 0, rest infinity
-	vertex = []
-	w = 0
-	e = 0
-	vis_d = [False]*len(g)	#initialize all vertices as unvisited and insert them in queue
-	
-	for i in range(len(d)):
-		for j in range(len(g)):
-			graph[d[i]][j] = 0
-			graph[j][d[i]] = 0
-		
-	for i in range(len(g)):
-		heapq.heappush(vertex,(dist[i],i))
-	parent = [-1]*n 	#array to store the shortest path from source
-	while vertex:	#until all vertices are visited
-		wei,v = heapq.heappop(vertex)
-		if vis_d[v] == False:
-		   vis_d[v] = True
-		for i in range(len(g)):
-			if graph[v][i] > 0:		#connected nodes of current vertex
-				e = i 	#edge to connected node
-				w = graph[v][i] 	#weight of current and connected vertex
-			if dist[v] + w < dist[e]:
-				dist[e] = dist[v] + w
-				parent[e] = v
-				heapq.heappush(vertex,(dist[e],v))
-	path = []
-	path.append(source)
-	print("Path:")
-	path = path_print(parent,end,path)
-	print(path)
-	distance = 0
-	for k in range(len(path) - 1):
-		distance = distance + graph[path[k]][path[k+1]]
-	print(distance)
-	return dist[end]
 
-def path_print(parent,end,path):
-	if parent[end] == -1:
-		return
-	path_print(parent,parent[end],path)
-	path.append(end)
-	return path
+class ThreadingExample(object):
+    """ Threading example class
+    The run() method will be started and it will run in the background
+    until the application exits.
+    """
 
-n = int(input("Enter number of vertex:"))
-graph = []
-danger_nodes = [3]
+    def __init__(self, interval=1):
+        """ Constructor
+        :type interval: int
+        :param interval: Check interval, in seconds
+        """
+        self.interval = interval
 
-print("Enter the weights in matrix:")
-for i in range(n):
-	print(i,":",end="")
-	graph_temp = [int(arr_temp) for arr_temp in input().strip().split()]
-	graph.append(graph_temp)
-	
-#start = input("Enter start node:")
-goal = input("Enter goal node:")
-for i in range(n):
-  if int(goal) != i and (i not in danger_nodes):
-        print("Start node: ",i)
-        goal_ = dijkstra(graph,danger_nodes,int(i),int(goal))
-    
+        thread = threading.Thread(target=self.run, args=())
+        thread.daemon = True                            # Daemonize thread
+        thread.start()                                  # Start the execution
+
+    def run(self):
+        """ Method that runs forever """
+        while True:
+            # Do something
+            # print('Doing something imporant in the background')
+            run_arduino()
+            time.sleep(self.interval)
+
+
+
+
+
+def dijkstra(g, dngr, source, end):
+    dist = [100] * len(g)
+    dist[source] = 0  # distance source vertex = 0, rest infinity
+    vertex = []
+    w = 0
+    e = 0
+    vis_d = [False] * len(g)  # initialize all vertices as unvisited and insert them in queue
+
+    for i in range(len(dngr)):
+        for j in range(len(g)):
+            graph[dngr[i]][j] = 0
+            graph[j][dngr[i]] = 0
+
+    for i in range(len(g)):
+        heapq.heappush(vertex, (dist[i], i))
+    parent = [-1] * 6  # array to store the shortest path from source
+    while vertex:  # until all vertices are visited
+        wei, v = heapq.heappop(vertex)
+        if vis_d[v] == False:
+            vis_d[v] = True
+        for i in range(len(g)):
+            if graph[v][i] > 0:  # connected nodes of current vertex
+                e = i  # edge to connected node
+                w = graph[v][i]  # weight of current and connected vertex
+                if dist[v] + w < dist[e]:
+                    dist[e] = dist[v] + w
+                    parent[e] = v
+                    heapq.heappush(vertex, (dist[e], v))
+    path = []
+    path.append(source)
+    print("Path:")
+    path = path_print(parent, end, path)
+    print(path)
+    distance = 0
+    for k in range(len(path) - 1):
+        distance = distance + graph[path[k]][path[k + 1]]
+    print(distance)
+    return dist[end]
+
+
+def path_print(parent, end, path):
+    if parent[end] == -1:
+        return
+    path_print(parent, parent[end], path)
+    path.append(end)
+    return path
+
+
+def capture_image():
+    camera = cv2.VideoCapture(0)
+    while True:
+        return_value, image = camera.read()
+        cv2.imshow('Our Face Extractor', image)
+        cv2.imwrite('crowd' + '.jpg', image)
+        if cv2.waitKey(1) == 13:  # 13 is the Enter Key
+            break
+    camera.release()
+
+
+def run_arduino():
+    global danger_nodes
+    # arduino = serial.Serial("COM3",9600)
+    for i in range(1, 2):
+        # data = arduino.readline()
+        # data = data.decode('utf-8')
+        # data = ['Node:1', 'Node:2']
+        data = ['Node:1']
+        dnode = [s[5:] for s in data if 'Node:' in s]
+        dnode = list(set(map(int, dnode)))
+        # print(data)
+        danger_nodes.extend(dnode)
+        danger_nodes = list(set(danger_nodes))
+        # print("danger: ", danger_nodes)
+        time.sleep(0.5)
+
+
+n = None
+graph = list()
+danger_nodes = list()
+goal = None
+
+
+def main_input():
+    global n,goal
+    n = int(input("Enter number of vertex:"))
+    print("Enter the weights in matrix:")
+    for i in range(n):
+        print(i, ":", end="")
+        graph_temp = [int(arr_temp) for arr_temp in input().strip().split()]
+        graph.append(graph_temp)
+
+    goal = input("Enter goal node:")
+
+
+def main():
+    # danger_nodes=[3]
+    global n, goal
+    for i in range(n):
+        if int(goal) != i and (i not in danger_nodes):
+            print("Start node: ", i)
+            goal_ = dijkstra(graph, danger_nodes, int(i), int(goal))
+
+
+if __name__ == '__main__':
+    example = ThreadingExample()
+    # time.sleep(30)
+    # global danger_nodes
+    capture_image()
+    dnode = inf.main()
+    danger_nodes.extend(dnode)
+    danger_nodes = list(set(danger_nodes))
+    print(danger_nodes)
+    main_input()
+    main()
+    print(danger_nodes)
+    # run_arduino()
+    # main()
+
+
+# 4
+# 0 1 2 0
+# 1 0 0 10
+# 2 0 0 5
+# 0 10 5 0
+# 3
+# 0
+
+# 6
+# 0 4 3 2 0 0
+# 4 0 0 2 2 0
+# 3 0 0 2 0 4
+# 2 2 2 0 1 0
+# 0 2 0 1 0 1
+# 0 0 4 0 1 0
+# 4
+# 0
